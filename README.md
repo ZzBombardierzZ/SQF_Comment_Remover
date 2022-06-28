1896f5fe8ba023bb4ec8ebc766cbc4bacb0177b9
Jeremiah <91098583+ZzBombardierzZ@users.noreply.github.com>
2022-06-28 11:20:26 -0700

    Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # SQF_Comment_Remover
-A few simple executables to remove comments, whitespace, and newlines from SQF code in 1 click.
+A few simple executables to remove comments, empty lines, and newlines from SQF code in 1 click.
+
+## Purpose
+The purpose of this project is to provide a simple way to remove comments, empty lines, and newlines from Arma 2 mission folders. This not only saves a lot of file size on your mission folder, but it also makes it harder for others to steal/understand your code.
+
+## Usage
+* In either usage you choose: before using the executable(s), I would make a backup of your mission folder before you start in case of any unforeseen bugs. While I don't believe any bugs still exist (I have tested this fairly thoroughly), it's always better to be safe than sorry.
+* My recommended usage:
+    * You should always have a local test server on your machine for testing and developing and then a dedicated one for your actual server (do not host the dedicated server on your own network...)
+    * Write a ton of comments in your sqf files to help you document your own work. This is a good habit to get into. There is no penalty for adding a ton of comments now as you will remove them in the coming steps.
+    * Now there are a few ways you can do this, but when you are ready to upload your current test server files to your dedicated machine, make a copy of your mission folder and run the desired executable(s) in the root of the copy, run it/them and then take them out of the mission folder. Then upload the copy to your dedicated server.
+        * To be clear, mission folder looks like `DayZ_Epoch_11.Chernarus`
+* Bare minimum usage:
+    * Stick the desired executable inside your mission folder. Now Run it.
+* What the files do:
+    * `Only_Remove_Comments.exe`: Removes comments from .sqf, .hpp, and .ext files.
+    * `Only_Remove_Emptylines.exe`: Removes empty lines from .sqf, .hpp, and .ext files.
+    * `Only_Remove_SQF_newlines.exe`: Removes all newlines from .sqf files.
+    * `Clean_it_all.exe`: (Does all of the above) Removes comments, empty lines, and newlines from .sqf files. It also removes comments and empty lines from .hpp and .ext files (not newlines).
+* Warning: If you accidentally run this in somewhere like the root of your C drive, it will likely run for a long time searching for sqf, hpp, and ext files in the entire drive and modify them respective to the exe you're using. So **DO NOT DO THAT**. While I __should__ have designed it to require to be within a MPMission folder, I'm not responsible for any damage you might cause.
+
+Let me know if you run into any issues or have any suggestions for the future. I hope you enjoy this tool.
+
+
+## Update History:
+* v1.0 - 6/28/22: Initial release.
+
+## Legal
+* You are welcome to use this code in any way you want.
+* I am not responsible for any damage you might cause.
+* I got the comment_remover code from [here](https://stackoverflow.com/a/18381470) and I do not claim credit for writing that part. Regex is powerful and I don't want to reinvent the wheel.
---
+++ myApp.py
@@ -0,0 +1,91 @@
+import re, os
+
+# I did not write the comment remover. I had a version I made but it had bugs. This was found on the internet.
+# https://stackoverflow.com/a/18381470
+def remove_comments(string):
+    pattern = r"(\".*?\"|\'.*?\')|(/\*.*?\*/|//[^\r\n]*$)"
+    # first group captures quoted strings (double or single)
+    # second group captures comments (//single-line or /* multi-line */)
+    regex = re.compile(pattern, re.MULTILINE|re.DOTALL)
+    def _replacer(match):
+        # if the 2nd group (capturing comments) is not None,
+        # it means we have captured a non-quoted (real) comment string.
+        if match.group(2) is not None:
+            return "" # so we will return empty to remove the comment
+        else: # otherwise, we will return the 1st group
+            return match.group(1) # captured quoted-string
+    return regex.sub(_replacer, string)
+
+
+
+def remove_all_newlines(data):
+    #currently has an issue with #defines. Set as false for now for hpp files
+    cleaned_data = ""
+    if len(data) > 0:
+        if data.find("\n") > -1:
+            for line in data.splitlines():
+                if line.find("#") == -1 and line.find(";") > -1:
+                    cleaned_data += line.rstrip() + " "
+                else :
+                    cleaned_data += line + "\n"
+        cleaned_data = re.sub(r'( #include)|(\t#include)', '\n#include', cleaned_data)
+        cleaned_data = re.sub(r'( #define)|(\t#define)', '\n#define', cleaned_data)
+    return cleaned_data
+
+def remove_empty_lines(data):
+    cleaned_data = ""
+    if len(data) > 0:
+        if data.find("\n") > -1:
+            for line in data.splitlines():
+                if len(line.rstrip())>0:
+                    cleaned_data += line + "\n"
+    return cleaned_data
+
+def clean_data_etc(file_path:str, remove_comments_bool:bool, remove_empty_lines_bool:bool, remove_newlines_bool:bool):
+    print("Cleaning: "+file_path)
+    
+    with open(file_path, "r") as f:
+        data = f.read()
+
+    cleaned_data = ""
+
+    if remove_comments_bool:
+        print("Removing Comments: "+file_path)
+        cleaned_data = remove_comments(data) # remove comments (new way found online)
+
+    if remove_empty_lines_bool:
+        print("Removing empty lines: "+file_path)
+        cleaned_data = remove_empty_lines(cleaned_data)
+
+    if remove_newlines_bool:
+        print("Removing new lines: "+file_path)
+        cleaned_data = remove_all_newlines(cleaned_data)
+
+    with open(file_path, "w") as f:
+        f.write(cleaned_data)
+
+def get_directory_files():
+    print("Getting directory paths of cwd")
+    path = os.getcwd()
+
+    this_directory_list = []
+
+    for root, dirs, files in os.walk(path, topdown=True):
+        for name in files:
+            this_directory_list.append(os.path.join(root, name))
+
+    print("This directory has these files:" + str(this_directory_list))
+    return this_directory_list
+
+def main_brain():
+    print("Starting Bomb's cleaning service")
+    directory_files = get_directory_files()
+    for file_path in directory_files:
+        if file_path.find(".hpp") > -1 or file_path.find(".ext") > -1:
+            clean_data_etc(file_path, True, True, False)
+        elif file_path.find(".sqf") > -1:
+            clean_data_etc(file_path, True, True, True)
+
+    print("Bomb's cleaning service has finished")
+
+main_brain()
